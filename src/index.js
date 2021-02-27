bde45d3f3bf4b71656d14465dfc6eac5984cc145
mbasart <mbd2797@gmail.com>
2021-02-27 22:31:58 +0100

    some changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Options = (props) => {
+  return (
+    <div>
+      <p>
+        {props.option} {props.value}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const nameTitle = 'give feedback'
+  const nameStati = 'statistics'
+  const optionGood = 'good'
+  const optionNeutral = 'neutral'
+  const optionBad = 'bad'
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <Header title={nameTitle} />
+      <Button handleClick={increaseGood} text='good' />
+      <Button handleClick={increaseNeutral} text='neutral' />
+      <Button handleClick={increaseBad} text='bad' />
+      <Header title={nameStati} />
+      <Options option={optionGood} value={good} />
+      <Options option={optionNeutral} value={neutral} />
+      <Options option={optionBad} value={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
